b98d435cc769184a7f40be28c92920a395e73d9a
Raminder Jeet <rjssodhi@gmail.com>
2015-03-12 12:03:21 -0400

    small bug

--- extension/background.js
+++ extension/background.js
@@ -34,7 +34,7 @@ chrome.browserAction.onClicked.addListener(function() {
     var left = (screen.width / 2) - ( w / 4);
     var top = (screen.height / 2) - (h / 4); 
 
-    chrome.windows.create({'url': 'canvas.html', 'type': 'popup', 'left': left, 'top': top, 'width': w, 'height': h,} , function(window) {
+    chrome.windows.create( {'url': 'canvas.html', 'type': 'popup', 'left': left, 'top': top, 'width': w, 'height': h} , function(window) {
     });
 });
 
