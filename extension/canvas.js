0cfcaffb39a7e8131f4f000e86e74bede77e2694
Raminder Jeet <rjssodhi@gmail.com>
2015-03-13 11:24:39 -0400

    Adding Rectangls and Line implementation

--- extension/canvas.js
+++ extension/canvas.js
@@ -2,28 +2,134 @@ var drawingCanvas = (function () {
 
 	"use strict";
 	var canvas,
-		ctx;
-
-	function mouseDownEvent(){
-			//testing mouse down event
-			ctx.fillRect(25,25,100,100);
-			ctx.clearRect(45,45,60,60);
-			ctx.strokeRect(50,50,50,50);
+		ctx,
+		mouse_x_coor,
+		mouse_y_coor,
+		started = false,
+		drawingtool,
+		drawingtools = {},
+		tool_selected = 'rect',
+		tool_default = 'line',
+		canvaso,
+		ctxo;
 		
+		drawingtools.line = function () {
+			var tool = this;
+			this.started = false;
+
+			this.mousedown = function (ev) {
+				tool.started = true;
+				tool.x0 = ev._x;
+				tool.y0 = ev._y;
+			};
+
+			this.mousemove = function (ev) {
+				if (!tool.started) {
+					return;
+				}
+		
+				ctx.clearRect(0, 31, canvas.width, canvas.height);
+				ctx.beginPath();
+				ctx.moveTo(tool.x0, tool.y0);
+				ctx.lineTo(ev._x, ev._y);
+				ctx.stroke();
+				ctx.closePath();
+			};
+
+			this.mouseup = function (ev) {
+				if (tool.started) {
+					tool.mousemove(ev);
+					tool.started = false;
+					img_update();
+				}
+			};
+		};
+		
+		drawingtools.rect = function () {
+			var tool = this;
+			this.started = false;
+
+			this.mousedown = function (ev) {
+				tool.started = true;
+				tool.x0 = ev._x;
+				tool.y0 = ev._y;
+			};
+
+			this.mousemove = function (ev) {
+				if (!tool.started) {
+					return;
+				}
+
+				var x = Math.min(ev._x,	tool.x0),
+				y = Math.min(ev._y,	tool.y0),
+				w = Math.abs(ev._x - tool.x0),
+				h = Math.abs(ev._y - tool.y0);
+
+				ctx.clearRect(0, 31, canvas.width, canvas.height);
+
+				if (!w || !h) {
+					return;
+				}
+
+				ctx.strokeRect(x, y, w, h);
+			};
+
+			this.mouseup = function (ev) {
+				if (tool.started) {
+					tool.mousemove(ev);
+					tool.started = false;
+					img_update();
+				}
+			};
+		};
+
+	
+	function mouseEvent(ev){
+ 
+		if (ev.layerX || ev.layerX == 0) { // Firefox
+			ev._x = ev.layerX;
+			ev._y = ev.layerY;
+		} 
+		
+		var func = drawingtool[ev.type];
+			if (func) {
+				func(ev);
+			}
+	}
+
+	function img_update () {
+		ctxo.drawImage(canvas, 0, 0);
+		ctx.clearRect(0, 31, canvas.width, canvas.height);
 	}
 
 	function drawCanvas(){
-			canvas = document.getElementById('Canvas');
+			canvaso = document.getElementById('Canvas');
 		
-			if (canvas.getContext) {
-				ctx = canvas.getContext('2d');
+			if (canvaso.getContext) {
+				ctxo = canvaso.getContext('2d');
 
 				var rectImage = new Image();   // a
 				var lineImage = new Image();   // b 
 				var lineArrowImage = new Image();   // c 
 				var text = new Image();   // d 
-				var eraser = new Image();   // e 
-			// Load images
+				var eraser = new Image();   // e
+
+				var container = canvaso.parentNode;
+				
+				canvas = document.createElement('canvas');
+				if (!canvas) {
+					alert('Error: I cannot create a new <canvas> element!');
+					return;
+				}
+
+				canvas.id = 'imageTemp';
+				canvas.width = canvaso.width;
+				canvas.height = canvaso.height;
+				container.appendChild(canvas);
+
+				ctx = canvas.getContext('2d');		
+				
+				// Load images
 				rectImage.src = "images/rect.png";
 				rectImage.onload = function() {
 				ctx.drawImage(rectImage, 0, 0, 30, 30);
@@ -56,11 +162,17 @@ var drawingCanvas = (function () {
 				ctx.stroke();
 			}
 	
-			canvas.addEventListener("mousedown", mouseDownEvent, true);
-			canvas.addEventListener("mousemove", mouseMoveEvent, false);
-			canvas.addEventListener("mouseup", mouseReleaseEvent);
-			canvas.addEventListener("mouseout", mouseOutEvent, false);
+
+		    if (drawingtools[tool_default]) {
+				drawingtool = new drawingtools[tool_default]();
+				drawingtool.value = tool_default;
+			}
 			
+			canvas.addEventListener("mousedown", mouseEvent, true);
+			canvas.addEventListener("mousemove", mouseEvent, false);
+			canvas.addEventListener("mouseup", mouseEvent);
+			canvas.addEventListener("mouseout", mouseEvent, false);
+
 	}
  
 	return {
