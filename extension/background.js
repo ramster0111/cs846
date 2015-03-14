23ef9d5dd6013454d350badf13d7402da9a90e8d
azer89 <azer.darkblade@gmail.com>
2015-03-14 16:38:15 -0400

    popup

--- extension/background.js
+++ extension/background.js
@@ -2,27 +2,31 @@
 // background.js cannot interact with DOM, use content.js instead
 
 
-function doStuffWithDOM(element) 
-{
-	if(element != null)
-	{
-        // content of the source code here
-        //alert(element);
-    }
-}
+//function doStuffWithDOM(element) 
+//{
+//	if(element != null)
+//	{
+//        // content of the source code here
+//        //alert(element);
+//    }
+//}
 
 chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab)
 {
-	if(tab.url.match(/https:\/\/(gist.)?github.com/))
+    var splitString = tab.url.split( '/' );
+	if(tab.url.match(/https:\/\/(gist.)?github.com/) && splitString.length >= 5 && splitString[5].match("edit"))
 	{
-
-		var splitString = tab.url.split( '/' );
-		if(splitString.length >= 5 && splitString[5].match("edit"))
-		{
-			// retrieving dom
-			chrome.tabs.sendMessage(tab.id, { text: "report_back" }, doStuffWithDOM);
-		}
+		// retrieving dom
+		//chrome.tabs.sendMessage(tab.id, { text: "report_back" }, doStuffWithDOM);
+        chrome.browserAction.enable(tabID);
+        chrome.browserAction.setIcon({path: "images/icon16.png"});
 	}
+    else
+    {
+        console.log("3");
+        chrome.browserAction.disable(tabID);
+        chrome.browserAction.setIcon({path: "images/icon16disable.png"});
+    }
 });
 
 
