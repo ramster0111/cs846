2906ec4a2569ee4db995622927ac9464ab2ab71a
azer89 <azer.darkblade@gmail.com>
2015-03-13 20:02:05 -0400

    editing code

---
+++ extension/converter.js
@@ -0,0 +1,29 @@
+
+// list of characters
+
+var vLine = "┃";
+var hLine = "━";
+
+var ulCorner = "┏";
+var urCorner = "┓";
+
+var blCorner = "┗";
+var brCorner = "┛";
+
+var rFork = "┣";
+var lFork = "┫";
+var bFork = "┳";
+
+var tFork = "┻";
+
+var intersection = "╋";
+
+var rTriangle = "▶"; 
+var lTriangle = "◀"; 
+var tTriangle = "▲";
+var bTriangle = "▼";
+
+
+var slash = "╱";
+var backslash = "╲";
+var cross = "╳";"
