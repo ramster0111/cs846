1f72f7a449b072205136a7be4437602e662e3367
Raminder Jeet <rjssodhi@gmail.com>
2015-03-15 17:31:44 -0400

    Fix arrowline and line to grid.

--- extension/canvas.js
+++ extension/canvas.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function ()
 		drawingtool,
 		drawingtools = {},
 		tool_selected = 'rect',
-		tool_default = 'line',
+		tool_default = 'arrowline',
 		canvaso,
 		rectangles = [],
 		lines = [],
@@ -64,6 +64,9 @@ var drawingCanvas = (function () {
 				tool.started = true;
 				tool.x0 = ev._x;
 				tool.y0 = ev._y;
+				tool.x0 = Math.round(tool.x0 / 10) * 10;
+				tool.y0 = Math.round(tool.y0 / 10) * 10;
+
 			};
 
 			this.mousemove = function (ev) {
@@ -72,11 +75,46 @@ var drawingCanvas = (function () {
 				}
 		
 				ctx.clearRect(0, 31, canvas.width, canvas.height);
-				ctx.beginPath();
+				ev._x = Math.round(ev._x / 10) * 10;
+				ev._y = Math.round(ev._y / 10) * 10;
+
+				
+// We have to draw two lines
+				if (Math.abs(ev._x - tool.x0) > Math.abs(ev._y - tool.y0)) {
+					ctx.beginPath();
+					ctx.moveTo(tool.x0, tool.y0);
+					ctx.lineTo(ev._x, tool.y0);
+					ctx.stroke();
+					ctx.closePath();
+					
+					ctx.beginPath();
+					ctx.moveTo(ev._x, tool.y0);
+					ctx.lineTo(ev._x, ev._y);
+					ctx.stroke();
+					ctx.closePath();
+				}
+
+				if (Math.abs(ev._x - tool.x0) < Math.abs(ev._y - tool.y0)) {
+					ctx.beginPath();
+					ctx.moveTo(tool.x0, tool.y0);
+					ctx.lineTo(tool.x0, ev._y);
+					ctx.stroke();
+					ctx.closePath();
+					
+					ctx.beginPath();
+					ctx.moveTo(tool.x0, ev._y);
+					ctx.lineTo(ev._x,ev._y);
+					ctx.stroke();
+					ctx.closePath();
+				}
+
+
+				/*
 				ctx.moveTo(tool.x0, tool.y0);
 				ctx.lineTo(ev._x, ev._y);
 				ctx.stroke();
 				ctx.closePath();
+				*/
 			};
 
 			this.mouseup = function (ev) {
@@ -85,8 +123,17 @@ var drawingCanvas = (function () {
 					var storelineobject = {};
 					storelineobject.x0 = tool.x0;
 					storelineobject.y0 = tool.y0;
-					storelineobject.x1 = ev._x;
-					storelineobject.y1 = ev._y;
+					if (Math.abs(ev._x - tool.x0) > Math.abs(ev._y - tool.y0)){
+							storelineobject.x1 = ev._x
+							storelineobject.y1 = tool.y0
+	
+					}
+					else if (Math.abs(ev._x - tool.x0) < Math.abs(ev._y - tool.y0)){
+							storelineobject.x1 = tool.x0
+							storelineobject.y1 = ev._y
+					}
+					storelineobject.x2 = ev._x;
+					storelineobject.y2 = ev._y;
 					lines.push(storelineobject);
 					tool.started = false;
 					img_update();
@@ -104,6 +151,9 @@ var drawingCanvas = (function () {
 				tool.started = true;
 				tool.x0 = ev._x;
 				tool.y0 = ev._y;
+				tool.x0 = Math.round(tool.x0 / 10) * 10;
+				tool.y0 = Math.round(tool.y0 / 10) * 10;
+
 			};
 
 			this.mousemove = function (ev) {
@@ -112,12 +162,73 @@ var drawingCanvas = (function () {
 				}
 		
 				ctx.clearRect(0, 31, canvas.width, canvas.height);
+				ev._x = Math.round(ev._x / 10) * 10;
+				ev._y = Math.round(ev._y / 10) * 10;
+
+				if (Math.abs(ev._x - tool.x0) > Math.abs(ev._y - tool.y0)) {
+					ctx.beginPath();
+					ctx.moveTo(tool.x0, tool.y0);
+					ctx.lineTo(ev._x, tool.y0);
+					ctx.stroke();
+					ctx.closePath();
+					
+					ctx.beginPath();
+					ctx.moveTo(ev._x, tool.y0);
+					ctx.lineTo(ev._x, ev._y);
+					ctx.stroke();
+					ctx.closePath();
+					
+					var endRadians = Math.atan((ev._y - tool.y0)/(ev._x - ev._x));
+					endRadians += ((ev._x >= ev._x) ? 90 : -90 ) * Math.PI / 180;
+
+					ctx.save();
+					ctx.beginPath();
+					ctx.translate(ev._x,ev._y);
+					ctx.rotate(endRadians);
+					ctx.moveTo(0,0);
+					ctx.lineTo(5,10);
+					ctx.lineTo(-5,10);
+					ctx.closePath();
+					ctx.restore();
+					ctx.fill();		
+				}
+
+				if (Math.abs(ev._x - tool.x0) < Math.abs(ev._y - tool.y0)) {
+					ctx.beginPath();
+					ctx.moveTo(tool.x0, tool.y0);
+					ctx.lineTo(tool.x0, ev._y);
+					ctx.stroke();
+					ctx.closePath();
+					
+					ctx.beginPath();
+					ctx.moveTo(tool.x0, ev._y);
+					ctx.lineTo(ev._x,ev._y);
+					ctx.stroke();
+					ctx.closePath();
+
+					var endRadians = Math.atan((ev._y - ev._y)/(ev._x - tool.x0));
+					endRadians += ((ev._x >= tool.x0) ? 90 : -90 ) * Math.PI / 180;
+
+					ctx.save();
+					ctx.beginPath();
+					ctx.translate(ev._x,ev._y);
+					ctx.rotate(endRadians);
+					ctx.moveTo(0,0);
+					ctx.lineTo(5,10);
+					ctx.lineTo(-5,10);
+					ctx.closePath();
+					ctx.restore();
+					ctx.fill();		
+					
+				}
+				/*
 				ctx.beginPath();
 				ctx.moveTo(tool.x0, tool.y0);
 				ctx.lineTo(ev._x, ev._y);
 				ctx.stroke();
 				ctx.closePath();
-				
+*/
+/*				
 				var endRadians = Math.atan((ev._y - tool.y0)/(ev._x - tool.x0));
 				endRadians += ((ev._x >= tool.x0) ? 90 : -90 ) * Math.PI / 180;
 
@@ -131,6 +242,7 @@ var drawingCanvas = (function () {
 				ctx.closePath();
 				ctx.restore();
 				ctx.fill();		
+*/
 			};
 
 			this.mouseup = function (ev) {
@@ -139,11 +251,21 @@ var drawingCanvas = (function () {
 					var storearrowlineobject = {};
 					storearrowlineobject.x0 = tool.x0;
 					storearrowlineobject.y0 = tool.y0;
-					storearrowlineobject.x1 = ev._x;
-					storearrowlineobject.y1 = ev._y;
+
+					if (Math.abs(ev._x - tool.x0) >= Math.abs(ev._y - tool.y0)){
+							storearrowlineobject.x1 = ev._x
+							storearrowlineobject.y1 = tool.y0
+	
+					}
+					else if (Math.abs(ev._x - tool.x0) < Math.abs(ev._y - tool.y0)){
+							storearrowlineobject.x1 = tool.x0
+							storearrowlineobject.y1 = ev._y
+					}					
+					storearrowlineobject.x2 = ev._x;
+					storearrowlineobject.y2 = ev._y;
 					storearrowlineobject.xdiff = 5;
-					storearrowlineobject.ydiff = 20;
-					lines.push(storearrowlineobject);
+					storearrowlineobject.ydiff = 10;
+					arrowlines.push(storearrowlineobject);
 					tool.started = false;
 					img_update();
 				}
@@ -172,6 +294,12 @@ var drawingCanvas = (function () {
 				y = Math.min(ev._y,	tool.y0);
 				w = Math.abs(ev._x - tool.x0);
 				h = Math.abs(ev._y - tool.y0);
+				
+				x = Math.round(x / 10) * 10;
+				y = Math.round(y / 10) * 10;
+				w = Math.round(w / 10) * 10;
+				h = Math.round(h / 10) * 10;
+
 
 				ctx.clearRect(0, 31, canvas.width, canvas.height);
 
@@ -242,8 +370,32 @@ var drawingCanvas = (function () {
 	}
 
 	function img_update () {
+	
+	/*
+		for (var x = 0; x < 601; x += 10) {
+			//ctx.save();
+				ctx.setLineDash([2]);
+				ctx.strokeRect(x, 30, 20, 20);
+			//ctx.restore();
+		}
+	*/
 		ctxo.drawImage(canvas, 0, 0);
 		ctx.clearRect(0, 31, canvas.width, canvas.height);
+		
+/* Lines not working ???		
+		for (var x = 0.5; x < 601; x += 10) {
+			ctx.beginPath();
+			ctx.moveTo(x, 0);
+			ctx.lineTo(x, 381);
+			ctx.closePath();
+		}
+
+		for (var y = 30.5; y < 301; y += 10) {
+			ctx.moveTo(30, y);
+			ctx.lineTo(500, y);
+		}
+*/		
+		
 	}
 
 	function drawCanvas()
@@ -253,6 +405,28 @@ var drawingCanvas = (function () {
 			if (canvaso.getContext) {
 				ctxo = canvaso.getContext('2d');
 
+				for (var x = 0.5; x < 601; x += 10) {
+					ctxo.save();
+					ctxo.setLineDash([1,2]);
+					ctxo.beginPath();
+					ctxo.moveTo(x,31);
+					ctxo.lineTo(x,300);
+					ctxo.stroke();
+				}
+
+				for (var y = 31.5; y < 301; y += 10) {
+					ctxo.save();
+					ctxo.setLineDash([1,2]);
+					ctxo.beginPath();
+					ctxo.moveTo(0,y);
+					ctxo.lineTo(600,y);
+					ctxo.stroke();
+				}
+
+				//ctxo.strokeRect(x, 30, 20, 20);
+			//ctx.restore();
+				
+				
 				var rectImage = new Image();   // a
 				var lineImage = new Image();   // b 
 				var lineArrowImage = new Image();   // c 
@@ -301,10 +475,10 @@ var drawingCanvas = (function () {
 					ctx.drawImage(eraser, 120, 0, 30, 30);
 				}
 	
-				ctx.beginPath();
-				ctx.moveTo(0, 31);
-				ctx.lineTo(600, 31);
-				ctx.stroke();
+				//ctx.beginPath();
+				//ctx.moveTo(0, 31);
+				//ctx.lineTo(600, 31);
+				//ctx.stroke();
 			}
 	
 
