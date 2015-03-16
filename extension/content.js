2164bc22902fcbb5c1d44869983785e970580c15
azer89 <azer.darkblade@gmail.com>
2015-03-16 11:58:15 -0400

    edited converter.js

--- extension/content.js
+++ extension/content.js
@@ -10,9 +10,19 @@ function mouseDown(e)
 {
 }
 
+function InsertANewline()
+{
+    var scriptContent = "document.querySelector('.ace_editor').env.editor.splitLine();\ndocument.querySelector('.ace_editor').env.editor.navigateDown(1);";
+    var script = document.createElement('script');
+    script.id = 'tmpScript';
+    script.appendChild(document.createTextNode(scriptContent));
+    document.body.appendChild(script);
+    $("#tmpScript").remove();
+}
+
 function InsertToAceEditor(message)
 {
-    var scriptContent = "document.querySelector('.ace_editor').env.editor.insert('" +  message+ " ')";
+    var scriptContent = "document.querySelector('.ace_editor').env.editor.insert('" +  message + " ')";
     var script = document.createElement('script');
     script.id = 'tmpScript';
     script.appendChild(document.createTextNode(scriptContent));
@@ -26,9 +36,11 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse)
 {
     if(msg.text && (msg.text == "insert_text")) 
     {
-        //InsertToAceEditor("HELLO WORLD");
-        //alert(msg.data.rectangles.length + " - " + msg.data.lines.length + " - " + msg.data.arrowlines.length);
-     
+        var stringToInsert = ParseVectorToUnicode(msg.data);
+        //alert(stringToInsert);
+        //InsertToAceEditor(stringToInsert);
+        InsertANewline();
+
         //alert(msg.data);
         //alert("insert_text 2");
         //sendResponse(document.getElementById("blob_contents").value);
--- extension/converter.js
+++ extension/converter.js
@@ -27,16 +27,67 @@ var slash = "╱";
 var backslash = "╲";
 var cross = "╳";
 
+var oriWidth = 600;
+var oriHeight = 300;
+var gridSize = 10;
+
 // not done yet
 // textInput: 	input text which contains unicode art
-function ParseTextToVector(dataInput)
+function ParseUnicodeToVector()
 {
 	//var width = 60;
 	//var height = 30
+
 }
 
 // not done yet
-function ParseVectorToText()
+function ParseVectorToUnicode(dataInput)
 {
+	//alert(dataInput.rectangles.length + " - " + dataInput.lines.length + " - " + dataInput.arrowlines.length);
+	var debugString = "";
+	var rectangles 	= dataInput.rectangles;
+	var lines 		= dataInput.lines;
+	var arrowlines 	= dataInput.arrowlines;
 
+	var returnString = "/* |";
+
+	var textWidth = oriWidth / gridSize;
+	var textHeight = oriHeight / gridSize;
+	for(a = 0; a < textHeight; a++)
+	{
+		for(b = 0; b < textWidth; b++)
+		{
+			returnString += "-";
+		}
+		returnString += " | ";
+	}
+	returnString += "*/"
+
+
+
+	// parse rectangles
+	for(i = 0; i < rectangles.length; i++)
+	{		
+		var rect = rectangles[i];
+		var xStart = rect.x;
+		var yStart = rect.y;
+		//debugString += rect.x + " - " + rect.y + " - " + rect.w + " - " + rect.h + "\n"; 
+	}
+
+	// parse lines
+	for(i = 0; i < lines.length; i++)
+	{		
+		var line = lines[i];
+		//debugString += line.x0 + " - " + line.y0 + " | " + line.x1 + " - " + line.y1 + " | " + line.x2 + " - " + line.y2 + "\n";
+	}
+
+	// parse arrow lines
+	for(i = 0; i < arrowlines.length; i++)
+	{		
+		var aline = arrowlines[i];
+		//debugString += aline.x0 + " - " + aline.y0 + " | " + aline.x1 + " - " + aline.y1 + " | " + aline.x2 + " - " + aline.y2 + "\n";
+	}
+
+	return returnString;
+	//alert(debugString);
 }
