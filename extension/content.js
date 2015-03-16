d5547e4a354dedf91e4061b2c0182fedd4e5286e
azer89 <azer.darkblade@gmail.com>
2015-03-16 14:38:01 -0400

    edited converter.js

--- extension/content.js
+++ extension/content.js
@@ -10,14 +10,55 @@ function mouseDown(e)
 {
 }
 
-function InsertANewline()
+// function AddASpace()
+// {
+//     var scriptContent = "document.querySelector('.ace_editor').env.editor.insert(' ')";
+//     var script = document.createElement('script');
+//     script.id = 'tmpScript';
+//     script.appendChild(document.createTextNode(scriptContent));
+//     document.body.appendChild(script);
+//     $("#tmpScript").remove();
+// }
+
+
+
+function NavigateLeft()
 {
-    var scriptContent = "document.querySelector('.ace_editor').env.editor.splitLine();\ndocument.querySelector('.ace_editor').env.editor.navigateDown(1);";
+    var scriptContent = "document.querySelector('.ace_editor').env.editor.navigateLeft(1);";
     var script = document.createElement('script');
     script.id = 'tmpScript';
     script.appendChild(document.createTextNode(scriptContent));
     document.body.appendChild(script);
     $("#tmpScript").remove();
+}
+
+function NavigateRight()
+{
+    var scriptContent = "document.querySelector('.ace_editor').env.editor.navigateRight(1);";
+    var script = document.createElement('script');
+    script.id = 'tmpScript';
+    script.appendChild(document.createTextNode(scriptContent));
+    document.body.appendChild(script);
+    $("#tmpScript").remove();
+}
+
+function InsertANewline()
+{
+    var scriptContent = "document.querySelector('.ace_editor').env.editor.insert('\\n'); ";
+    var script = document.createElement('script');
+    script.id = 'tmpScript';
+    script.appendChild(document.createTextNode(scriptContent));
+    document.body.appendChild(script);
+    $("#tmpScript").remove();
+
+    /*
+    var scriptContent = "document.querySelector('.ace_editor').env.editor.splitLine(); document.querySelector('.ace_editor').env.editor.navigateDown(1);";
+    var script = document.createElement('script');
+    script.id = 'tmpScript';
+    script.appendChild(document.createTextNode(scriptContent));
+    document.body.appendChild(script);
+    $("#tmpScript").remove();
+    */
 }
 
 function InsertToAceEditor(message)
@@ -30,16 +71,54 @@ function InsertToAceEditor(message)
     $("#tmpScript").remove();
 }
 
+// function InsertToAceEditorWithSpaces(message)
+// {
+//     AddASpace();
+//     AddASpace();
+//     NavigateLeft();
+//     InsertToAceEditor(message);
+
+// }
+
+
+
+// function ReplaceAllInEditor(replacement, charStr)
+// {
+//     var scriptContent = "document.querySelector('.ace_editor').env.editor.replaceAll('" + replacement +"', '" + charStr + "')";
+//     var script = document.createElement('script');
+//     script.id = 'tmpScript';
+//     script.appendChild(document.createTextNode(scriptContent));
+//     document.body.appendChild(script);
+//     $("#tmpScript").remove();
+// }
+
 
 // Listen for messages
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) 
 {
     if(msg.text && (msg.text == "insert_text")) 
     {
-        var stringToInsert = ParseVectorToUnicode(msg.data);
+        var stringArray = ParseVectorToUnicode(msg.data);
+        //InsertToAceEditor( stringArray);
+//
         //alert(stringToInsert);
         //InsertToAceEditor(stringToInsert);
+
         InsertANewline();
+        InsertToAceEditor("/*");
+
+        console.log(stringArray.length);
+        for(a = 0; a < stringArray.length; a++)
+        {
+           InsertANewline();
+           InsertToAceEditor( "* " + stringArray[a]);
+        }
+
+
+        InsertANewline();
+        InsertToAceEditor("*/");
+
+        //DummyFUnction();
 
         //alert(msg.data);
         //alert("insert_text 2");
--- extension/converter.js
+++ extension/converter.js
@@ -40,6 +40,30 @@ function ParseUnicodeToVector()
 
 }
 
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+function ParseVectorToUnicode1(dataInput)
+{
+	var textWidth = oriWidth / gridSize;
+	var textHeight = oriHeight / gridSize;
+	var stringArray = "/*\\n";
+
+	for(a = 0; a < textHeight; a++)
+	{
+		for(b = 0; b < textWidth; b++)
+		{
+			stringArray += " ";
+		}
+		stringArray += "\\n";
+	}
+	stringArray += "*/";
+	console.log(textWidth + " --- " + textHeight + " --- " + stringArray.length);
+	return stringArray;
+}
+
 // not done yet
 function ParseVectorToUnicode(dataInput)
 {
@@ -49,31 +73,72 @@ function ParseVectorToUnicode(dataInput)
 	var lines 		= dataInput.lines;
 	var arrowlines 	= dataInput.arrowlines;
 
-	var returnString = "/* |";
+
+	/*
+	var testString = "BONANA";
+	testString = setCharAt(testString,1,'a');
+	alert(testString);*/
+
 
 	var textWidth = oriWidth / gridSize;
 	var textHeight = oriHeight / gridSize;
+	var stringArray = [];
+	console.log(textWidth + " --- " + textHeight);
+	for(a = 0; a < textHeight; a++)
+	{
+		var lineStr = "";
+		for(b = 0; b < textWidth; b++)
+		{
+			lineStr += " ";
+		}
+		stringArray.push(lineStr);
+	}
+
+	/*
 	for(a = 0; a < textHeight; a++)
 	{
 		for(b = 0; b < textWidth; b++)
 		{
-			returnString += "-";
+			stringArray[a] += "-";
 		}
-		returnString += " | ";
 	}
-	returnString += "*/"
+	*/
 
-
+	/*
+	for(a = 0; a < textHeight; a++)
+	{
+		for(b = 0; b < textWidth; b++)
+		{
+			stringArray[a] = setCharAt(stringArray[a],b,'~');
+		}
+	}
+	*/
 
 	// parse rectangles
+	/*
 	for(i = 0; i < rectangles.length; i++)
 	{		
 		var rect = rectangles[i];
-		var xStart = rect.x;
-		var yStart = rect.y;
 		//debugString += rect.x + " - " + rect.y + " - " + rect.w + " - " + rect.h + "\n"; 
-	}
 
+		var x1 = rect.x / gridSize;
+		var x2 = (rect.x + rect.w)  / gridSize;
+		for(a = rect.y / gridSize; a < (rect.y + rect.h) / gridSize; a++)
+		{
+			stringArray[a] = setCharAt(stringArray[a],x1, vLine);
+			stringArray[a] = setCharAt(stringArray[a],x2, vLine);
+		}
+
+		var y1 = rect.y  / gridSize;
+		var y2 = (rect.y + rect.h)  / gridSize;
+		for(a = rect.x / gridSize; a < (rect.x + rect.w) / gridSize; a++)
+		{
+			stringArray[y1] = setCharAt(stringArray[y1],x, hLine);
+			stringArray[y2] = setCharAt(stringArray[y2],x, hLine);
+		}
+
+	}*/
+	/*
 	// parse lines
 	for(i = 0; i < lines.length; i++)
 	{		
@@ -87,7 +152,14 @@ function ParseVectorToUnicode(dataInput)
 		var aline = arrowlines[i];
 		//debugString += aline.x0 + " - " + aline.y0 + " | " + aline.x1 + " - " + aline.y1 + " | " + aline.x2 + " - " + aline.y2 + "\n";
 	}
+	*/
 
-	return returnString;
+	// for(a = 0; a < textHeight; a++)
+	// {
+		
+	// 	stringArray[a] = " *" + stringArray[a];
+	// }
+
+	return stringArray;
 	//alert(debugString);
 }
