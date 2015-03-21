ecd537d9345103eeab7df371edfc9b94e6a69ed7
azer89 <shadow_of_darkblade@yahoo.co.id>
2015-03-21 14:28:02 -0400

    Update architecture.c

--- architecture.c
+++ architecture.c
@@ -1,5 +1,33 @@
 
 /*
+/* 
+*    ┏━━━━━━━━━━━┓            ┏━━━━━━━━━━━━━┓                   
+*    ┃           ┃            ┃             ┃                   
+*    ┃  index    ┃            ┃   server    ┃                   
+*    ┃           ┃            ┃             ┃                   
+*    ┃           ┃   search   ┃             ┃                   
+*    ┃           ◀━━━━━━━━━━━━┫             ┃                   
+*    ┃           ┃            ┃             ┃                   
+*    ┃           ┃            ┃             ┃                   
+*    ┃           ┃ return results           ┃                   
+*    ┃           ┣━━━━━━━━━━━━▶             ┃                   
+*    ┃           ┃            ┃             ┃                   
+*    ┗━━━━━━▲━━━━┛            ┗━━━━━▲━━━┳━━━┛                   
+*           ┃                       ┃   ┃                       
+*           ┃                  gets ┃   ┃gives                  
+*           ┃                  query┃   ┃results                
+*  adds new documents to            ┃   ┃                       
+*      ┏━━━━┻━━━━━━┓          ┏━━━━━┻━━━▼━━━━━┓                 
+*      ┃           ┃          ┃               ┃                 
+*      ┃  crawler  ┃          ┃      UI       ┃                 
+*      ┃           ┃          ┃               ┃                 
+*      ┃           ┃          ┃               ┃                 
+*      ┃           ┃          ┃               ┃                 
+*      ┃           ┃          ┃               ┃                 
+*      ┃           ┃          ┃               ┃                 
+*      ┃           ┃          ┗━━━━━━━━━━━━━━━┛                 
+*      ┗━━━━━━━━━━━┛                                            
+*/ 
 Draw an architecture diagram.
 For example, operating system, database, web service.
 The diagram should have 3 components and interaction.
