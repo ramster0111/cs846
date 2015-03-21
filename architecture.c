98953e976f2c3767797a6b6beb4e62882f11e139
azer89 <shadow_of_darkblade@yahoo.co.id>
2015-03-21 15:41:26 -0400

    Update architecture.c

--- architecture.c
+++ architecture.c
@@ -1,33 +1,6 @@
 
 
-/* 
-*                    Apac┏e Cassa┓dr┏ St┓rage Layers            
-*                        ┃       ┃  ┃   ┃                       
-*            Write━━━━━▶ ┃       ┃  ┃   ┃                       
-*                        ┃       ┃  ┃   ┃                       
-*                        ┃       ┃  Full Memtable               
-*                        ┃       ┃  ┃   ┃                       
-*                        ┃       ┃  ┃   ┃                       
-*                        ┃       ┃  ┃   ┃                       
-*                        ┃       ┃  ┃ ┃ ┃                       
-*    In-Memory:          ┃       ┃  ┃ ┃ ┃                       
-*                        ┃       ┃  ┃ ┃ ┃                       
-*                        ┗━━━━━━━┛  ┗━╋━┛                       
-*                         MemTable    ┃Flush                    
-*                ━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━     
-*                                     ┃                         
-*                                     ┃                         
-*                                     ┃   SSTables              
-*                                  ┏━━▼━━━┳━━━━━┳━━━━━┓         
-*                                  ┃      ┃     ┃     ┃         
-*                                  ┃      ┃     ┃     ┃         
-*    Disc Storage:                 ┃      ┃     ┃     ┃         
-*                                  ┃      ┃     ┃     ┃         
-*                                  ┃      ┃     ┃     ┃         
-*                                  ┃      ┃     ┃     ┃         
-*                                  ┃      ┃     ┃     ┃         
-*                                  ┗━━━━━━┻━━━━━┻━━━━━┛         
-*/ 
+/*
 Draw an architecture diagram.
 For example, operating system, database, web service.
 The diagram should have 3 components and interaction.
