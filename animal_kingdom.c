ffeec628d04e42c4a37e799f3ff42b4c0ef88752
azer89 <shadow_of_darkblade@yahoo.co.id>
2015-03-21 16:09:51 -0400

    Update animal_kingdom.c

--- animal_kingdom.c
+++ animal_kingdom.c
@@ -1,5 +1,30 @@
 /*
 
+/* 
+*                      ┏━━━━━━━━━┓                              
+*                      ┃         ┃                              
+*                      ┃         ┃                              
+*                      ┃  apple  ┃                              
+*                      ┃         ┃                              
+*                      ┗━━━━▲━▲━━┛                              
+*                           ┃ ┃                                 
+*                           ┃ ┃                                 
+*                      ┏━━━━┛ ┗━━━━━┓                           
+*                      ┃            ┃                           
+*                      ┃            ┃                           
+*                      ┃          ┏━┻━━━━━━━━━━┓                
+*             ┏━━━━━━━━┻━━━┓      ┃            ┃                
+*             ┃            ┃      ┃            ┃                
+*             ┃green apple ┃      ┃ red apples ┃                
+*             ┗▲━━━━━━━━━━━┛      ┗━▲━━━━━━━━▲━┛                
+*              ┃                    ┃        ┃                  
+*              ┃                    ┃        ┃                  
+*              ┃                    ┃        ┃                  
+*     ┏━━━━━━━━┻┓      ┏━━━━━━━━━━━━┻━┓    ┏━┻━━━━━━━━━━━┓      
+*     ┃         ┃      ┃              ┃    ┃             ┃      
+*     ┃  Fiji   ┃      ┃  honey crisp ┃    ┃red delicious┃      
+*     ┗━━━━━━━━━┛      ┗━━━━━━━━━━━━━━┛    ┗━━━━━━━━━━━━━┛      
+*/ 
 Task:
 Draw a class diagram for animal kingdom.
 At least 3 classes with properties and interactions.
