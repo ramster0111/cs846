30a74aa1aec2100fe9828c57a8d3bdd7bf3dd562
azer89 <shadow_of_darkblade@yahoo.co.id>
2015-03-21 16:41:02 -0400

    Update list.c

--- list.c
+++ list.c
@@ -1,22 +1,7 @@
 
 // DRAW A DIAGRAM FOr THIS SOURCE CODE
 
-/* 
-*                                ┏━━┳━━━━┓       ┏━━┳━━━━┓      
-*                                ┃  ┃    ┃       ┃  ┃    ┃      
-*                                ┃  ┃    ┃       ┃  ┃    ┃      
-*                   head ━━━━━━━━▶  ┃next┣━━━━━━━▶  ┃null┃      
-*                                ┃  ┃    ┃       ┃  ┃    ┃      
-*                                ┃  ┃    ┃       ┃  ┃    ┃      
-*                                ┗━━┛▲━━━┛       ┗━━┻━━━━┛      
-*                                    ┃                          
-*                                    ┃                          
-*                                    ┃                          
-*                                    ┃                          
-*                                                               
-*                                                               
-*                                 current                       
-*/ 
+
 
 // http://www.macs.hw.ac.uk/~rjp/Coursewww/Cwww/linklist.html
 #include<stdlib.h>
